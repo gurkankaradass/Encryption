5db6f3b1415e8d3a7c9ec930f7531f9015ebebd0
Gürkan Karadaş <149390886+gurkankaradass@users.noreply.github.com>
2024-05-15 13:22:25 +0300

    Encryption Application Created

---
+++ app.js
@@ -0,0 +1,36 @@
+const leftText = document.querySelector("#leftTextarea");
+const rightText = document.querySelector("#rightTextarea");
+const encodeButton = document.querySelector("#encode");
+const decodeButton = document.querySelector("#decode");
+const cleanButton = document.querySelector("#clean");
+
+runEventListener();
+
+function runEventListener(){
+    encodeButton.addEventListener("click", encode);
+    decodeButton.addEventListener("click", decode);
+    cleanButton.addEventListener("click", clear)
+}
+
+function encode(){
+    if(leftText.value==""){
+        alert("Kriptolonacak Şifreyi Giriniz");
+        return;
+    }
+    rightText.value = btoa(leftText.value);
+    leftText.value = "";
+}
+
+function decode(){
+    if(rightText.value==""){
+        alert("Çözülecek Şifreyi Giriniz");
+        
+    }
+    leftText.value = atob(rightText.value);
+    rightText.value="";
+}
+
+function clear(){
+    leftText.value = "";
+    rightText.value="";
+}
